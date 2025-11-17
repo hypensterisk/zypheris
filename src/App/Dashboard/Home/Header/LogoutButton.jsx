e4bb3e682ba6d7a2712c8996fe95e443b1f17c08
hypensterisk <roykhanalaushaf0@gmail.com>
2025-11-17 06:51:59 +0000

    feat(Dashboard): add conditional automatic redirection
    - Redirect to `/setup` if no database is found
    - Redirect to `/unlock` if no password, preserving target path via `continue` state
    - Keep existing routes for Home and Card components

--- src/App/Dashboard/Home/Header/LogoutButton.jsx
+++ src/App/Dashboard/Home/Header/LogoutButton.jsx
@@ -2,17 +2,14 @@
 
 import useDatabaseStore from '@hooks/useDatabaseStore.js'
 import Nav from 'react-bootstrap/Nav'
-import { useNavigate } from 'react-router'
 
 export default function LogoutButton() {
   const { setData, setPassword } = useDatabaseStore((state) => state)
-  const navigate = useNavigate()
   return (
     <Nav.Link
       onClick={() => {
         setData(null)
         setPassword('')
-        navigate('/unlock')
       }}
     >
       <i className='fa-solid fa-arrow-right-from-bracket' /> Logout
--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,11 +1,23 @@
 /** @format */
 
-import { Routes, Route } from 'react-router'
+import useDatabaseStore from '@hooks/useDatabaseStore'
+import { Routes, Route, Navigate, useLocation } from 'react-router'
 
 import Card from './Card/index.jsx'
 import Home from './Home/index.jsx'
 
 export default function Dashboard() {
+  const location = useLocation()
+  const database = useDatabaseStore((state) => state.database)
+  const password = useDatabaseStore((state) => state.password)
+  if (!database) return <Navigate to='/setup' />
+  if (!password)
+    return (
+      <Navigate
+        to='/unlock'
+        state={{ continue: location.pathname }}
+      />
+    )
   return (
     <Routes>
       <Route
